4d62e61892c57607eee208bf9e46a22341527fae
lightunderwolft <22521013@gm.uit.edu.vn>
2024-04-30 14:33:45 +0700

    update UI home(chat and profile)

--- Form3.cs
+++ Form3.cs
@@ -15,11 +15,28 @@ namespace NT106_project
         public Form3()
         {
             InitializeComponent();
+            chatbox.Visible = true;
+            Menubox.Visible = true;
+            profilebox.Visible = false;
         }
 
         private void guna2Button1_Click(object sender, EventArgs e)
         {
+            Menubox.Visible = true;
+            profilebox.Visible = true;
+            guna2Button1.ShadowDecoration.Enabled = true;
+            guna2Button2.ShadowDecoration.Enabled = false;
+            guna2Button3.ShadowDecoration.Enabled = false;
+        }
 
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            Menubox.Visible = true;
+            profilebox.Visible = false;
+            chatbox.Visible = true;
+            guna2Button3.ShadowDecoration.Enabled = true;
+            guna2Button1.ShadowDecoration.Enabled = false;
+            guna2Button2.ShadowDecoration.Enabled = false;
         }
     }
 }
